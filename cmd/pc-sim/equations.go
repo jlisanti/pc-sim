ec3b70798bf41dddf3b3a943c7b9f8c29dec1749
Joel Lisanti <joel.c.lisanti@gmail.com>
2022-01-17 16:36:00 -0500

    reorg

--- cmd/pc-sim/equations.go
+++ cmd/pc-sim/equations.go
@@ -1,7 +1,7 @@
-package finitedifference
+package main
 
-import "github.com/jlisanti/pc-sim/internal/finitedifference"
+import "github.com/jlisanti/pc-sim/internal/cfd"
 
-func F1(i, int, dz float64, rho []float64, rhoU []float64, rhoE []float64) []float64 {
-	return finitedifference.Forward1(i, rhoU, dz)
+func F1(i int, dz float64, rho []float64, rhoU []float64, rhoE []float64) []float64 {
+	return cfd.Forward1(i, rhoU, dz)
 }
