1545dea0502f41508f1529f9432df7253ed733c2
Joel Lisanti <joel.c.lisanti@gmail.com>
2022-01-17 19:08:22 -0500

    added 1st order functions

--- cmd/pc-sim/equations.go
+++ cmd/pc-sim/equations.go
@@ -8,30 +8,50 @@ import (
 
 func F1(i int, cGrid cfd.Grid) float64 {
 	// Forward difference
-	value := (cGrid.RhoU[i+1]*cGrid.Area[i+1] - cGrid.RhoU[i]*cGrid.Area[i]) / cGrid.Dz
-	return value
+	A := 0.0
+	if i > int(cGrid.Points)-1 {
+		A = (cGrid.RhoU[i]*cGrid.Area[i] - cGrid.RhoU[i-1]*cGrid.Area[i-1]) / cGrid.Dz
+	} else {
+		A = (cGrid.RhoU[i+1]*cGrid.Area[i+1] - cGrid.RhoU[i]*cGrid.Area[i]) / cGrid.Dz
+	}
+	return A
 }
 
 func F2(i int, cGrid cfd.Grid) float64 {
-	// Forward difference
-	A := cGrid.Rho[i+1]*math.Pow(cGrid.RhoU[i+1]/cGrid.Rho[i+1], 2) + cGrid.P[i+1]*cGrid.Area[i+1]
-	B := cGrid.Rho[i+1]*math.Pow(cGrid.RhoU[i+1]/cGrid.Rho[i+1], 2) + cGrid.P[i+1]*cGrid.Area[i+1]
-	C := cGrid.P[i] * ((cGrid.Area[i+1] - cGrid.Area[i]) / cGrid.Dz)
-	D := cGrid.Rho[i] * cGrid.Area[i] * ((4.0 * cGrid.Fric) / cGrid.ID[i]) * math.Pow(cGrid.U[i], 2) * 0.5 * cGrid.U[i] * (1.0 / math.Abs(cGrid.U[i]))
+	A, B, C, D := 0.0, 0.0, 0.0, 0.0
+	if i > int(cGrid.Points)-1 {
+		// Backward difference
+		A = cGrid.Rho[i]*math.Pow(cGrid.RhoU[i+1]/cGrid.Rho[i], 2) + cGrid.P[i]*cGrid.Area[i]
+		B = cGrid.Rho[i-1]*math.Pow(cGrid.RhoU[i-1]/cGrid.Rho[i-1], 2) + cGrid.P[i-1]*cGrid.Area[i-1]
+		C = cGrid.P[i] * ((cGrid.Area[i] - cGrid.Area[i-1]) / cGrid.Dz)
+		D = cGrid.Rho[i] * cGrid.Area[i] * ((4.0 * cGrid.Fric) / cGrid.ID[i]) * math.Pow(cGrid.U[i], 2) * 0.5 * cGrid.U[i] * (1.0 / math.Abs(cGrid.U[i]))
 
-	value := ((A - B) / cGrid.Dz) + C - D
+	} else {
+		// Forward difference
+		A = cGrid.Rho[i+1]*math.Pow(cGrid.RhoU[i+1]/cGrid.Rho[i+1], 2) + cGrid.P[i+1]*cGrid.Area[i+1]
+		B = cGrid.Rho[i]*math.Pow(cGrid.RhoU[i]/cGrid.Rho[i], 2) + cGrid.P[i]*cGrid.Area[i]
+		C = cGrid.P[i] * ((cGrid.Area[i+1] - cGrid.Area[i]) / cGrid.Dz)
+		D = cGrid.Rho[i] * cGrid.Area[i] * ((4.0 * cGrid.Fric) / cGrid.ID[i]) * math.Pow(cGrid.U[i], 2) * 0.5 * cGrid.U[i] * (1.0 / math.Abs(cGrid.U[i]))
+	}
 
-	return value
+	return ((A - B) / cGrid.Dz) + C - D
 }
 
 func F3(i int, cGrid cfd.Grid) float64 {
-	// Forward difference
-	A := cGrid.U[i+1] * (cGrid.Rho[i+1]*cGrid.Area[i+1]*cGrid.E[i+1] + cGrid.P[i+1]*cGrid.Area[i+1])
-	B := cGrid.U[i] * (cGrid.Rho[i]*cGrid.Area[i]*cGrid.E[i] + cGrid.P[i]*cGrid.Area[i])
-	C := 0.0 // Q value
-	D := 4.0 * cGrid.ID[i] * cGrid.H * (cGrid.T[i] - cGrid.Tair)
+	A, B, C, D := 0.0, 0.0, 0.0, 0.0
+	if i > int(cGrid.Points)-1 {
+		// Backward difference
+		A = cGrid.U[i] * (cGrid.Rho[i]*cGrid.Area[i]*cGrid.E[i] + cGrid.P[i]*cGrid.Area[i])
+		B = cGrid.U[i-1] * (cGrid.Rho[i-1]*cGrid.Area[i-1]*cGrid.E[i-1] + cGrid.P[i-1]*cGrid.Area[i-1])
+		C = 0.0 // Q value
+		D = 4.0 * cGrid.ID[i] * cGrid.H * (cGrid.T[i] - cGrid.Tair)
+	} else {
+		// Forward difference
+		A = cGrid.U[i+1] * (cGrid.Rho[i+1]*cGrid.Area[i+1]*cGrid.E[i+1] + cGrid.P[i+1]*cGrid.Area[i+1])
+		B = cGrid.U[i] * (cGrid.Rho[i]*cGrid.Area[i]*cGrid.E[i] + cGrid.P[i]*cGrid.Area[i])
+		C = 0.0 // Q value
+		D = 4.0 * cGrid.ID[i] * cGrid.H * (cGrid.T[i] - cGrid.Tair)
+	}
 
-	value := ((A - B) / cGrid.Dz) + C + D
-
-	return value
+	return ((A - B) / cGrid.Dz) + C + D
 }
--- internal/cfd/grid.go
+++ internal/cfd/grid.go
@@ -1,5 +1,7 @@
 package cfd
 
+import "math"
+
 type Grid struct {
 	Points int64
 	Area   []float64
@@ -15,6 +17,8 @@ type Grid struct {
 	Tair   float64
 	Fric   float64
 	H      float64
+	Cv     float64
+	R      float64
 }
 
 func NewGrid(length int64) *Grid {
@@ -26,11 +30,18 @@ func UpdateSubStep(i int, tGrid Grid, ki float64, li float64, ni float64) {
 	tGrid.Rho[i] = tGrid.Rho[i] + 0.5*ki
 	tGrid.RhoU[i] = tGrid.RhoU[i] + 0.5*li
 	tGrid.RhoE[i] = tGrid.RhoE[i] + 0.5*ni
+	tGrid.U[i] = tGrid.RhoU[i] / tGrid.Rho[i]
+	tGrid.E[i] = tGrid.RhoE[i] / tGrid.Rho[i]
+	tGrid.T[i] = (1.0 / tGrid.Cv) * (tGrid.E[i] - math.Pow(tGrid.U[i], 2))
+	tGrid.P[i] = tGrid.Rho[i] * tGrid.R * tGrid.T[i]
 }
 
 func UpdateStep(i int, tGrid Grid, ki [4]float64, li [4]float64, ni [4]float64) {
 	tGrid.Rho[i] = tGrid.Rho[i] + (1.0/6.0)*(ki[0]+2.0*ki[1]+2.0*ki[2]+ki[3])
 	tGrid.RhoU[i] = tGrid.RhoU[i] + (1.0/6.0)*(li[0]+2.0*li[1]+2.0*li[2]+li[3])
 	tGrid.RhoE[i] = tGrid.RhoE[i] + (1.0/6.0)*(ni[0]+2.0*ni[1]+2.0*ni[2]+ni[3])
-
+	tGrid.U[i] = tGrid.RhoU[i] / tGrid.Rho[i]
+	tGrid.E[i] = tGrid.RhoE[i] / tGrid.Rho[i]
+	tGrid.T[i] = (1.0 / tGrid.Cv) * (tGrid.E[i] - math.Pow(tGrid.U[i], 2))
+	tGrid.P[i] = tGrid.Rho[i] * tGrid.R * tGrid.T[i]
 }
